12f7c5ac9e1bab9de261f1859d73f8a8b67dc17e
santhosh <santhoshwork2919@gmail.com>
2026-05-12 12:59:41 +0530

    Delete front end/FRONT-END directory

--- front end/FRONT-END/JavascriptDOMpractice/index.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById("pass");
-const butt = document.getElementById("toggle");
-
-butt.onclick =() =>{
-    const isHidden = input.type === "password";
-    input.type = isHidden ? "text": "password";
-    butt.textContent = isHidden ? "Hide" : "Show";
-};
