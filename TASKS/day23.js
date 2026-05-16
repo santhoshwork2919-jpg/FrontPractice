83dc328420727ac180407128e37310fd58436ced
santhosh <santhoshwork2919@gmail.com>
2026-05-16 20:51:39 +0530

    Add files via upload

---
+++ TASKS/day23.js
@@ -0,0 +1,26 @@
+// print the num using loops
+
+for(let i = 0;i <= 20;i++)
+{
+console.log(i);
+};
+
+//check the number the even
+
+let num = 55;
+
+if(num % 2 == 0){
+    console.log(`${num} is a even number` );
+}
+else{
+    console.log(`${num} is odd number`)
+}
+ //sum value
+
+ let sum = 0 ;
+
+ for(let i = 0 ; i <= 300 ; i++)
+ {
+    sum = sum + i;
+ }
+ console.log(sum);
